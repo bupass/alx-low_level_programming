cdfbafec5744f24213817dcbfff57260cd4e8a32
symon <symonabok@gmail.com>
2023-06-20 05:00:43 -0700

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,1 @@
-#ifndef main.h
-#define main.h
-
-int -putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isapha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_baure(void);
-void times_table(void)
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-
-#endif
+int _putchar(char c);
