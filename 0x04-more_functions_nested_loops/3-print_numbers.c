814498dfff6017fdb9f8875af69c966258ca1a1a
symon <symonabok@gmail.com>
2023-06-22 12:26:02 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
-/* Declaration of _putchar function */
-int _putchar(char c);
-
 /**
- * print_numbers - Prints the numbers from 0 to 9, followed by a new line.
- */
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
+*/
+
 void print_numbers(void)
 {
-    int i;
+	char c;
 
-    for (i = 0; i < 10; i++)
-        _putchar(i + '0');
-
-    _putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
 }
