082b3e2d334be9ddfadc4e0dd5d422abc28607a6
symon <symonabok@gmail.com>
2023-06-29 10:42:32 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+char *leet(char *s);
+
+int main(void)
+{
+    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+    char *p;
+
+    p = leet(s);
+    printf("%s", p);
+    printf("%s", s);
+    return 0;
+}
+
+char *leet(char *s)
+{
+    /* Implementation of the 'leet' function */
+    /* ... */
+    return s;
+}
